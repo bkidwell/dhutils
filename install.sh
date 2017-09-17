c4a2f06e732b3daeb427dcba0a287450cbfcf4fc
Brendan Kidwell <brendan_kidwell@abtassoc.com>
2017-09-17 19:27:36 -0400

    fixing permissions

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
-mkdir -p ~/dhutils/conf
-chmod 770 ~/dhutils/conf
-if [ ! -f ~/dhutils/conf/db_backup.conf.php ]; then
-    cp ~/dhutils/templates/db_backup.conf.php.example ~/dhutils/conf/db_backup.conf.php
+cd ~/dhutils
+
+git config core.fileMode false
+chmod +x db_backup.php
+chmod +x rdiff-backup
+chmod +x rdiff-backup.files/rdiff-backup
+chmod +x rdiff-backup.files/rdiff-backup.statistics
+
+mkdir -p conf
+chmod 770 conf
+if [ ! -f conf/db_backup.conf.php ]; then
+    cp templates/db_backup.conf.php.example conf/db_backup.conf.php
 fi
 
-cp ~/dhutils/templates/bash_profile ~/.bash_profile
+cp templates/bash_profile ~/.bash_profile
 chmod 644 ~/.bash_profile
 echo Installed .bash_profile. Please log out and log back in for .bash_profile to take effect.
