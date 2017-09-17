16e37102e6eac8142011f544b3697fdc40c31e5a
Brendan Kidwell <brendan_kidwell@abtassoc.com>
2017-09-17 19:09:57 -0400

    tweak

--- install.sh
+++ install.sh
@@ -7,6 +7,4 @@ fi
 
 cp ~/dhutils/templates/bash_profile ~/.bash_profile
 chmod 644 ~/.bash_profile
-echo Installed .bash_profile. Please log back in.
-
-logout
+echo Installed .bash_profile. Please log out and log back in for .bash_profile to take effect.
